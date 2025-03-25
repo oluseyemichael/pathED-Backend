b3e9e5047e9f2a035c997a36b0c107aa84ab7096
oluseyemichael <oluseyemichael11@gmail.com>
2025-03-25 23:10:35 +0200

    Bug fix

--- learning_platform/learning_platform/urls.py
+++ learning_platform/learning_platform/urls.py
@@ -58,7 +58,7 @@ urlpatterns = [
     path('api/v1/quizzes/<int:quiz_id>/submit/', submit_quiz, name='submit_quiz'),
     path('api/v1/module-progress', get_module_progress, name='get_module_progress'),
     path('api/v1/next-learning-path/<int:current_learning_path_id>/', get_next_learning_path, name='get_next_learning_path'),
-    path("generate-learning-paths/", generate_learning_paths, name="generate_learning_paths"),
+    path("api/v1/generate-learning-paths/", generate_learning_paths, name="generate_learning_paths"),
     # Versioned router endpoint
     path('api/v1/', include(router.urls)),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
