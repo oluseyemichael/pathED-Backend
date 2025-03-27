474191b01613c733c29702d8627c92c7f0c852cf
oluseyemichael <oluseyemichael11@gmail.com>
2025-03-27 12:20:09 +0200

    Added invalidate cache url

--- learning_platform/learning_platform/urls.py
+++ learning_platform/learning_platform/urls.py
@@ -13,7 +13,7 @@ from core.views import (
     CourseViewSet, ModuleViewSet, QuizViewSet, LearningPathViewSet, 
     RegisterView, LoginView, VerifyEmailView, PasswordResetRequestView,
     PasswordResetConfirmView, ModuleProgressViewSet, QuizProgressViewSet,
-    CourseProgressViewSet, get_user_profile, update_course_progress, get_module_by_name, submit_quiz, LearningPathProgressViewSet, get_module_progress, get_next_learning_path, generate_quiz_from_video, generate_learning_paths
+    CourseProgressViewSet, get_user_profile, update_course_progress, get_module_by_name, submit_quiz, LearningPathProgressViewSet, get_module_progress, get_next_learning_path, generate_quiz_from_video, generate_learning_paths, invalidate_learning_path_cache
 )
 
 # Versioned Router Setup for API
@@ -59,6 +59,9 @@ urlpatterns = [
     path('api/v1/module-progress', get_module_progress, name='get_module_progress'),
     path('api/v1/next-learning-path/<int:current_learning_path_id>/', get_next_learning_path, name='get_next_learning_path'),
     path("api/v1/generate-learning-paths/", generate_learning_paths, name="generate_learning_paths"),
+    path(
+        "api/v1/invalidate-learning-path-cache/", invalidate_learning_path_cache, name="invalidate_cache"
+    ),
     # Versioned router endpoint
     path('api/v1/', include(router.urls)),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
